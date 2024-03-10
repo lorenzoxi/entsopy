361d13edd0c80f882a117588bcacaaf4bb3fea57
Lorenzo Perinello <perinellolor@gmail.com>
2024-03-10 10:03:31 +0100

    v 2.0.1

--- entsopy/components/welcome.py
+++ entsopy/components/welcome.py
@@ -1,21 +1,15 @@
 from rich.panel import Panel
 from rich import print
-import toml 
+import entsopy
 
 def welcome_panel() -> None:
     """
     Displays a welcome panel with information about ENTSOPY and a link to the official entso-e website.
     """
-    
-    # Reading the TOML file
-    with open("pyproject.toml", 'r') as toml_file:
-        data = toml.load(toml_file)
-    
-    version = data['tool']['poetry']['version']
-    
+        
     print(
         Panel(
-            f"Welcome to [cornflower_blue]ENTSOPY[/cornflower_blue] {version}: your assistant for downloading data from entso-e "
+            f"Welcome to [cornflower_blue]ENTSOPY[/cornflower_blue]: your assistant for downloading data from entso-e "
             "transparency platform.\nVisit the official entso-e website here: ["
             "link=https://transparency.entsoe.eu/]transparency.entsoe.eu[/link]",
             style="white",
